407b0f27a30a6ebc1f4053531a13d876cc21991f
DCotton <me@andywalpole.me>
2022-04-10 18:53:27 +0100

    new slug for the markdown library

--- apps/haroldhill/pages/articles/[slug].ts
+++ apps/haroldhill/pages/articles/[slug].ts
@@ -1,5 +1,6 @@
 import fs from 'fs';
-import { join } from 'path';
+import {join} from 'path';
+import {getParsedFileContentBySlug} from '@libs/markdown'
 
 interface ArticleProps extends ParsedUrlQuery {
   slug: string;
@@ -7,11 +8,27 @@ interface ArticleProps extends ParsedUrlQuery {
 
 const POSTS_PATH = join(process.cwd(), '_articles');
 
+export const getStaticProps: GetStaticProps<ArticleProps> = async ({ params }: {
+  params: ArticleProps;
+}) => {
+  // read markdown file into content and frontmatter
+  const articleMarkdownContent = getParsedFileContentBySlug(
+    params.slug,
+    POSTS_PATH
+  );
+
+  return {
+    props: {
+      slug: params.slug,
+    },
+  };
+};
+
 export const getStaticPaths: GetStaticPaths<ArticleProps> = async () => {
   const paths = fs
     .readdirSync(POSTS_PATH)
     .map((path) => path.replace(/\.mdx?$/, '')) // Remove file extensions for page paths
-    .map((slug) => ({ params: { slug } })); // Map the path into the static paths object required by Next.js
+    .map((slug) => ({params: {slug}})); // Map the path into the static paths object required by Next.js
 
   return {
     paths,
--- libs/markdown/src/lib/markdown.ts
+++ libs/markdown/src/lib/markdown.ts
@@ -1,11 +1,12 @@
 import fs from 'fs';
 import { join } from 'path';
 import matter from 'gray-matter';
+import { MarkdownDocument } from './types';
 
 export const getParsedFileContentBySlug = (
   slug: string,
   postsPath: string
-) => {
+): MarkdownDocument => {
 
   const postFilePath = join(postsPath, `${slug}.mdx`);
   const fileContents = fs.readFileSync(postFilePath);
