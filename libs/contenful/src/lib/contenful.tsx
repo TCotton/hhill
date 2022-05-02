dd5891e0d560fa59cca74eeb62b454cb4ef5a3ab
DCotton <me@andywalpole.me>
2022-05-02 18:44:17 +0100

    add more markdown npm packages

--- libs/contenful/src/lib/contenful.tsx
+++ libs/contenful/src/lib/contenful.tsx
@@ -1,12 +1,12 @@
 import { ContentfulClient } from 'react-contentful';
 
 export const contentfulClient = ContentfulClient({
-  space: process.env.NX_CONTENTFUL_PROD_SPACE_ID || '7n7vfqlomamo',
-  accessToken: process.env.ACCESS_TOKEN,
+  space: process.env['NX_CONTENTFUL_PROD_SPACE_ID'] || '',
+  accessToken: process.env['ACCESS_TOKEN'] || '',
 });
 
 export const previewContentfulClient = ContentfulClient({
-  space: process.env.NX_CONTENTFUL_PROD_SPACE_ID || '7n7vfqlomamo',
-  accessToken: process.env.PREVIEW_ACCESS_TOKEN,
+  space: process.env['NX_CONTENTFUL_PROD_SPACE_ID'] || '',
+  accessToken: process.env['PREVIEW_ACCESS_TOKEN'] || '',
   host: 'preview.contentful.com',
 });
