72270783fc241232b842744b28685d1dba070766
DCotton <me@andywalpole.me>
2023-01-21 14:48:52 +0000

    added styling to link on page

--- apps/haroldhill/components/navigation.tsx
+++ apps/haroldhill/components/navigation.tsx
@@ -13,36 +13,42 @@ const NavigationMenuDemo = () => {
             Menu <CaretDownIcon className={styles.CaretDown} aria-hidden />
           </NavigationMenu.Trigger>
           <NavigationMenu.Content>
-            <ul className="govuk-list govuk-list--spaced">
+            <ul className="govuk-list govuk-list--spaced gem-c-layout-super-navigation-header__navigation-second-items">
               <ListItem
                 title="Introduction"
-                href="/docs/primitives/overview/introduction">
+                href="/docs/primitives/overview/introduction"
+                className="govuk-link gem-c-layout-super-navigation-header__navigation-second-item-link">
                 Build high-quality, accessible design systems and web apps.
               </ListItem>
               <ListItem
                 title="Getting started"
-                href="/docs/primitives/overview/getting-started">
+                href="/docs/primitives/overview/getting-started"
+                className="govuk-link gem-c-layout-super-navigation-header__navigation-second-item-link">
                 A quick tutorial to get you up and running with Radix
                 Primitives.
               </ListItem>
               <ListItem
                 title="Styling"
-                href="/docs/primitives/overview/styling">
+                href="/docs/primitives/overview/styling"
+                className="govuk-link gem-c-layout-super-navigation-header__navigation-second-item-link">
                 Unstyled and compatible with any styling solution.
               </ListItem>
               <ListItem
                 title="Animation"
-                href="/docs/primitives/overview/animation">
+                href="/docs/primitives/overview/animation"
+                className="govuk-link gem-c-layout-super-navigation-header__navigation-second-item-link">
                 Use CSS keyframes or any animation library of your choice.
               </ListItem>
               <ListItem
                 title="Accessibility"
-                href="/docs/primitives/overview/accessibility">
+                href="/docs/primitives/overview/accessibility"
+                className="govuk-link gem-c-layout-super-navigation-header__navigation-second-item-link">
                 Tested in a range of browsers and assistive technologies.
               </ListItem>
               <ListItem
                 title="Releases"
-                href="/docs/primitives/overview/releases">
+                href="/docs/primitives/overview/releases"
+                className="govuk-link gem-c-layout-super-navigation-header__navigation-second-item-link">
                 Radix Primitives releases and their changelogs.
               </ListItem>
             </ul>
@@ -72,7 +78,7 @@ type Ref = HTMLAnchorElement
 
 const ListItem = React.forwardRef<Ref, IForwardRefProps>(
   ({ className, children, title, ...props }, forwardedRef) => (
-    <li>
+    <li className="gem-c-layout-super-navigation-header__dropdown-list-item">
       <p className="govuk-link">
         <NavigationMenu.Link asChild>
           <a
