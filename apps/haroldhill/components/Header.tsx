dfdf386de03ba779fbd68956a36835ccbf6e9d52
DCotton <me@andywalpole.me>
2023-01-28 16:05:30 +0000

    fixed types for the MyLink component

--- apps/haroldhill/components/Header.tsx
+++ apps/haroldhill/components/Header.tsx
@@ -1,25 +1,36 @@
-import React from 'react'
+import React, { MouseEvent } from 'react'
 import MenuItems from './MenuItems'
 import Link from 'next/link'
 import styles from './Header.module.css'
 const Header = () => {
-  const MyLink = React.forwardRef(({ onClick, href }, ref) => {
-    return (
-      <a
-        href={href}
-        ref={ref}
-        className={`govuk-link govuk-link--inverse govuk-link--no-underline ${styles.header}`}>
-        {/* eslint-disable-next-line react/no-unescaped-entities */}
-        <em>HAROLD HILL</em>: A People's History{' '}
-      </a>
-    )
-  })
+  interface IMyLinkRefProps {
+    onClick?: MouseEvent<HTMLAnchorElement>
+    href?: string
+  }
+
+  type Ref = HTMLAnchorElement
+
+  const MyLink = React.forwardRef<Ref, IMyLinkRefProps>(
+    ({ onClick, href }, ref) => {
+      return (
+        <a
+          href={href}
+          ref={ref}
+          className={`govuk-link govuk-link--inverse govuk-link--no-underline ${styles.header}`}>
+          {/* eslint-disable-next-line react/no-unescaped-entities */}
+          <em>HAROLD HILL</em>: A People's History{' '}
+        </a>
+      )
+    }
+  )
   MyLink.displayName = 'MyButton'
+
   return (
     <header className="govuk-header " role="banner" data-module="govuk-header">
       <div className="govuk-header__container govuk-width-container">
         <div className="govuk-grid-row">
-          <div className={`govuk-grid-column-two-thirds-from-desktop ${styles.center}`}>
+          <div
+            className={`govuk-grid-column-two-thirds-from-desktop ${styles.center}`}>
             <p className={`govuk-heading-m ${styles.siteHeader}`}>
               <Link href="/" passHref legacyBehavior>
                 <MyLink />
