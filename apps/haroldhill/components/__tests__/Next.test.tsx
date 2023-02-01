a5eacf1bd7a5d4686a1635f866284c552e4d0f7e
DCotton <me@andywalpole.me>
2023-02-01 14:35:16 +0000

    props added

--- apps/haroldhill/components/__tests__/Next.test.tsx
+++ apps/haroldhill/components/__tests__/Next.test.tsx
@@ -1,8 +1,12 @@
 import Next from '../Next'
 import { render } from '@testing-library/react'
 describe('Next', () => {
+  const props = {
+    articleId: '12345'
+  }
+
   it('should render successfully', () => {
-    const { baseElement } = render(<Next />)
+    const { baseElement } = render(<Next {...props} />)
     expect(baseElement).toBeTruthy()
   })
 })
