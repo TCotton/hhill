5b6211f805d183e145dbfe46ce0215e73835044d
Andy Walpole <andywalpole@Andys-MacBook-Air.local>
2023-02-02 03:00:30 +0000

    tidy up ready for deployment

--- libs/types/src/lib/index.types.ts
+++ libs/types/src/lib/index.types.ts
@@ -1,4 +1,72 @@
-import { IChapters, IChaptersFields, IPagesFields, IPages } from './contentful'
+import { Entry } from 'contentful'
+
+export interface IChaptersFields {
+  /** title */
+  title?: string | undefined
+
+  /** slug */
+  slug?: string | undefined
+
+  /** pages */
+  pages: Entry<{ [fieldId: string]: unknown }>[]
+}
+
+export interface IChapters extends Entry<IChaptersFields> {
+  sys: {
+    id: string
+    type: string
+    createdAt: string
+    updatedAt: string
+    locale: string
+    contentType: {
+      sys: {
+        id: 'chapters'
+        linkType: 'ContentType'
+        type: 'Link'
+      }
+    }
+  }
+}
+
+export interface IPagesFields {
+  /** Title */
+  title?: string
+
+  /** slug */
+  slug?: string | undefined
+
+  /** content rich text */
+  contentRichText?: string
+
+  newId?: number | undefined
+}
+
+/** All pages here */
+
+export interface IPages extends Entry<IPagesFields> {
+  sys: {
+    id: string
+    type: string
+    createdAt: string
+    updatedAt: string
+    locale: string
+    contentType: {
+      sys: {
+        id: 'pages'
+        linkType: 'ContentType'
+        type: 'Link'
+      }
+    }
+  }
+}
+
+export type CONTENT_TYPE = 'chapters' | 'pages'
+
+export type IEntry = IChapters | IPages
+
+export type LOCALE_CODE = 'en-US'
+
+export type CONTENTFUL_DEFAULT_LOCALE_CODE = 'en-US'
 
 export interface IArticle {
   includes?: {
