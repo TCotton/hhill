4d036f52120e3188a9534007098f0ca366710d21
Andy Walpole <andywalpole@Andys-MacBook-Air.local>
2023-02-02 04:30:37 +0000

    removed reversal on navigation items

--- apps/haroldhill/components/MenuItems.tsx
+++ apps/haroldhill/components/MenuItems.tsx
@@ -4,23 +4,21 @@ import Navigation from './Navigation'
 const getArticles = async () => {
   const content = await fetch('http://localhost:3000/api/allArticles')
   const articles = await content.json()
-  return articles.result.items
-    .map((article) => {
-      return {
-        title: article.fields.title,
-        slug: article.fields.slug,
-        pages: article.fields.pages.map((page) => {
-          return {
-            title: page.fields.title,
-            contentRichText: page.fields.contentRichText,
-            slug: page.fields.slug,
-            parent: article.fields.title,
-            id: page.sys.id
-          }
-        })
-      }
-    })
-    .reverse()
+  return articles.result.items.map((article) => {
+    return {
+      title: article.fields.title,
+      slug: article.fields.slug,
+      pages: article.fields.pages.map((page) => {
+        return {
+          title: page.fields.title,
+          contentRichText: page.fields.contentRichText,
+          slug: page.fields.slug,
+          parent: article.fields.title,
+          id: page.sys.id
+        }
+      })
+    }
+  })
 }
 
 const MenuItems = () => {
