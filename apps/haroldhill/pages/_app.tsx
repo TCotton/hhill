12764ff61a02a9027b786673f10c29ac106b2bee
DCotton <me@andywalpole.me>
2023-02-13 09:50:43 +0000

    added reportWebVitals method

--- apps/haroldhill/pages/_app.tsx
+++ apps/haroldhill/pages/_app.tsx
@@ -1,15 +1,28 @@
 import React from 'react'
 import type { NextPage } from 'next'
 import { AppProps } from 'next/app'
+import type { NextWebVitalsMetric } from 'next/app'
 import { NextSeo } from 'next-seo'
 import '../styles/reset.scss'
 import '../styles/gds.scss'
 import '../styles/radixui.scss'
 import '../styles/gemc_navigation.scss'
-import { GoogleAnalytics } from 'nextjs-google-analytics'
+import { GoogleAnalytics, event } from 'nextjs-google-analytics'
 
 export type NextPageWithLayout = NextPage & {
   getLayout?: (page: React.ReactElement) => React.ReactNode
+}
+
+export function reportWebVitals(metric: NextWebVitalsMetric) {
+  event(metric.name, {
+    category:
+      metric.label === 'web-vital' ? 'Web Vitals' : 'Next.js custom metric',
+    value: Math.round(
+      metric.name === 'CLS' ? metric.value * 1000 : metric.value
+    ), // values must be integers
+    label: metric.id, // id unique to current page load
+    nonInteraction: true // avoids affecting bounce rate.
+  })
 }
 
 function CustomApp({ Component, pageProps }: AppProps) {
