09968fe8b8e4324c7bed309fe1e0385bb68388eb
DCotton <me@andywalpole.me>
2023-02-18 11:58:37 +0000

    added url to instructions

---
+++ apps/haroldhill/next-sitemap.config.js
@@ -0,0 +1,18 @@
+const siteUrl = 'https://jsonplaceholder.typicode.com/'
+// https://articles.wesionary.team/how-to-implement-sitemap-in-next-js-using-next-sitemap-77ca7bb56544
+module.exports = {
+  siteUrl,
+  generateRobotsTxt: true,
+  robotsTxtOptions: {
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/'
+      }
+    ],
+
+    additionalPaths: async (config) => [
+      await config.transform(config, '/comments')
+    ]
+  }
+}
