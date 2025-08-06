fbb95af7e30242dee4de272b899ca8f39cb15ab9
TCotton <me@andywalpole.me>
2025-08-06 15:59:01 +0100

    feat(hhill): added primitive cache to HTTP calls

--- apps/haroldhill/pages/api/allArticles.ts
+++ apps/haroldhill/pages/api/allArticles.ts
@@ -1,16 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next'
 import client from '../../helpers/contentful'
-
+let cachedData: any = null
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
+  if (cachedData) {
+    return res.status(200).json(cachedData)
+  }
   return client
     .getEntries({
       content_type: 'chapters'
     })
     .then((result) => {
-      res.status(200).json({
+      cachedData = {
         message: 'ok',
         result
-      })
+      }
+      res.status(200).json(cachedData)
     })
     .catch((err) => {
       res.status(500).json({
